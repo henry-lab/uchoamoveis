adf3379f09770d976d13388c2167f4493b69736f
Paulo Henrique dos Santos <henry.wddg@gmail.com>
2018-12-10 00:33:51 -0300

    uchoa version 0.01

---
+++ assets/js/uchoa-main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+  $('.owl-carousel').owlCarousel({
+    items:1,
+    loop:true,
+    margin:0,
+    autoplay:true,
+    autoplayTimeout:4000,
+    // autoplayHoverPause:true,
+    animateOut: 'fadeOut',
+    dots: false
+  });
+});
